6b04b52b0c3244d549045d93eeaf85dc2eec50f2
Ryan Ngai <ryanngai21@gmail.com>
2024-10-03 01:10:18 -0400

    Implementation of delete only by logged in users

--- tests/app_test.py
+++ tests/app_test.py
@@ -75,9 +75,15 @@ def test_messages(client):
 
 def test_delete_message(client):
     """Ensure the messages are being deleted"""
-    rv = client.get('/delete/1')
+    rv = client.get("/delete/1")
+    data = json.loads(rv.data)
+    assert data["status"] == 0
+    login(client, app.config["USERNAME"], app.config["PASSWORD"])
+    rv = client.get("/delete/1")
     data = json.loads(rv.data)
     assert data["status"] == 1
+
+
 
 def test_search_with_query(client):
     '''Create a test post'''
